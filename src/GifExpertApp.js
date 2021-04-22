66422652b328454032833ae796e061babc18e2a3
andres giraldo <drawl9988@gmail.com>
2021-04-21 21:54:30 -0500

    primer commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+
+export const GifExpertApp = () => {
+
+
+
+    const [categories, setCategories] = useState(['One punch']);
+
+    // const handleAdd = () => {
+
+    //     setCategories(['HunterxHunter', ...categories]);
+
+    // }
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+
+            <AddCategory setCategories={setCategories} />
+            <hr />
+
+            <ol>
+                {
+                    categories.map(category =>
+                        <GifGrid
+                            category={category}
+                            key={category} />
+                    )
+                }
+
+            </ol>
+        </>
+    )
+}
